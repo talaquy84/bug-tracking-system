04982a57b581fa1c81b51594d8c1a6210e9ce930
Eric Tran <34658353+talaquy84@users.noreply.github.com>
2021-03-06 12:35:00 -0800

    add redux and login feature, upgrade npm package

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 import userRoutes from './routes/userRoutes.js'
 import ticketRoutes from './routes/ticketRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 //Body parser
 app.use(express.json())
 
@@ -31,4 +36,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`.yellow.bold
-  ))  
+  ))
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,25 +2,43 @@ import React from 'react'
 import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap'
 import { Route } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 import SearchBox from './SearchBox'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar className='border border-secondary border-5' style={{ background: 'WhiteSmoke' }} expand='lg' collapseOnSelect>
-        <Navbar.Brand href='/home'>Bug Tracking </Navbar.Brand>
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Route render={({ history }) => <SearchBox history={history} />} />
+        <LinkContainer to='/home'>
+          <Navbar.Brand href='/home'>Bug Tracking</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav' >
+          {/* <Route render={({ history }) => <SearchBox history={history} />} /> */}
           <Nav className='ml-auto'>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Nav.Link href='/home'>Home</Nav.Link>
-            <Nav.Link href='/home'>Link</Nav.Link>
-            <NavDropdown title='Eric Profile' id='basic-nav-dropdown' >
-              <NavDropdown.Item href='/profile'>Profile</NavDropdown.Item>
+            <NavDropdown title={userInfo.name} id='username' >
+              <LinkContainer to='/profile'>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </LinkContainer>
               <NavDropdown.Item href='/setting'>Setting</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href='/logout'>Logout</NavDropdown.Item>
+              <LinkContainer to='/'>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </LinkContainer>
             </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
